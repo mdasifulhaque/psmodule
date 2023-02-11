062481c23406903542b47a6b71388eb0d27d086c
Md. Asiful Haque <asiful.bracu@gmail.com>
2023-02-12 00:57:10 +0600

    Created method ArrangeInput() and Added new parameter Append for log file append mode

--- RoboCMDL/RobocopyCmdlet/MoveWithRobocopy.cs
+++ RoboCMDL/RobocopyCmdlet/MoveWithRobocopy.cs
@@ -1,4 +1,5 @@
 ﻿using System.Management.Automation;
+using System.Security.AccessControl;
 
 namespace RoboCMDL;
 
@@ -12,13 +13,58 @@ public class MoveWithRobocopy : Cmdlet
     [Parameter(Mandatory = true, Position = 1, HelpMessage = "Destination Directory")]
     [Alias("TargetDirectory", "FinalPath")]
     public string Destination { get; set; }
-    [Parameter(Mandatory = true, Position = 2, HelpMessage = "Log File Location")]
+
+    [Parameter(Mandatory = false, Position = 2,
+        HelpMessage = "Provide the file name to copy. The default value is *.*")]
+    public string Filename { get; set; } = "*.*";
+
+    [Parameter(Mandatory = false, Position = 3, HelpMessage = "Log File Location")]
     public string LogFile { get; set; }
 
+    [Parameter(Mandatory = false, Position = 4, HelpMessage = "Do you want to append the log data?")]
+    public SwitchParameter Append { get; set; }
 
-    private string _logFilePath = "move_with_robocopy.log";
+    private string _logFile = "move_with_robocopy.log";
 
 
+    private string _argumentList = $"/A:-SH /E";
 
+    protected override void BeginProcessing()
+    {
+        base.BeginProcessing();
+        WriteObject("Starting files Operation");
+    }
+
+    protected override void ProcessRecord()
+    {
+
+        base.ProcessRecord();
+        ArrangeInput();
+    }
+
+    private string ProcessLogFile(string path)
+    {
+        var result = path;
+        var isPathDefault = string.Equals(path, "move_with_robocopy.log");
+        if (isPathDefault)
+        {
+            WriteObject("Going with Default Log File");
+        }
+        return result;
+    }
+
+
+    private void ArrangeInput()
+    {
+        _logFile = ProcessLogFile(LogFile);
+        if (Append.IsPresent)
+        {
+            _argumentList += $" /log:+{_logFile}";
+        }
+        else
+        {
+            _argumentList += $" /log:{_logFile}";
+        }
+    }
 
 }
